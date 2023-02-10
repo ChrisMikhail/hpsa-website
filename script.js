8e373e7a1bed1fbfbda8b9df66d50d87c51a3444
Chris Mikhail <samuelmikhc@gmail.com>
2023-02-09 23:57:33 -0500

    improved focus on menu when it is out

--- script.js
+++ script.js
@@ -1,34 +1,45 @@
+let total = 0;
+
 function respondNav() {
 
-    WindowWidth = $(window).width();
-  
-    if (WindowWidth <= 880) {
-      $("nav ul").css("display", "none");
-      $(".toggle-button").show();
-      $("nav ul").css("margin", "0");
-      $("nav ul li").css("text-align", "right");
-      $("nav ul li").css("transform", "translate(0, 8rem)");
-      $("nav ul li").css("margin-top", "3rem");
-    } else {
-      $(".toggle-button").hide();
-      $("nav ul").css("display", "inline");
-      $("nav ul li").css("display", "inline-block");
-      $("nav ul li").css("transform", "translate(0, 0)");
-      $("nav ul").css("background-color", "transparent");
-    }
+  WindowWidth = $(window).width();
+
+  if (WindowWidth <= 880) {
+    $("nav ul").css("display", "none");
+    $(".toggle-button").show();
+    $("nav ul").css("margin", "0");
+    $("nav ul li").css("text-align", "right");
+    $("nav ul li").css("transform", "translate(0, 8rem)");
+    $("nav ul li").css("margin-top", "3rem");
+  } else {
+    $(".toggle-button").hide();
+    $("nav ul").css("display", "inline");
+    $("nav ul li").css("display", "inline-block");
+    $("nav ul li").css("transform", "translate(0, 0)");
+    $("nav ul").css("background-color", "transparent");
+    $(".container").css("filter", "brightness(100%)");
+    total = 0;
   }
-  
-  $(document).ready(function () {
-    respondNav();
-  })
-  
-  $(window).resize(function () {
-    respondNav();
-  })
+}
+
+$(document).ready(function () {
+  respondNav();
+})
+
+$(window).resize(function () {
+  respondNav();
+})
 
 $(".toggle-button").click(function () {
-    $("nav ul li").css("display", "list-item");
-    $("nav ul").css("background-color", "rgb(38, 52, 80)");
-    $("nav ul").css("height", "100vh");
-    $("nav ul").animate({width: 'toggle'});
-  });
+  total++;
+  $("nav ul li").css("display", "list-item");
+  $("nav ul").css("background-color", "rgb(38, 52, 80)");
+  $("nav ul").css("height", "100vh");
+  $("nav ul").animate({ width: 'toggle' }, 200);
+  $(".container").css("filter", "brightness(40%)");
+  $("body").css("overflow-y", "hidden");
+  if (total % 2 == 0) {
+    $(".container").css("filter", "brightness(100%)");
+    $("body").css("overflow-y", "visible");
+  }
+});
