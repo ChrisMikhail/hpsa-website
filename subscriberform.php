b6ecdf73c668ae199b299b7900a7196132a5971d
Chris Mikhail <samuelmikhc@gmail.com>
2023-02-23 18:17:03 -0500

    Made changes to responsiveness

---
+++ subscriberform.php
@@ -0,0 +1,16 @@
+<?php
+
+
+    if($_POST["email"]) {
+
+
+        mail("akshay2334@hotmail.com", "New HPSA Email Subscription",
+
+
+        $_POST["insert your message here"]. "From: samuelmikhc@gmail.com");
+
+
+    }
+
+
+?>
